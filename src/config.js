d3d3d80f0e596a7f950ffe44b875a23fe312afa7
Akhrorbekmaccore <akhrorbek20011707@gmail.com>
2021-07-11 03:54:09 +0500

    problem fixed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const host = 'localhost'
-const PORT = 7000
+const PORT = process.env.PORT || 7000
 
 const pgConfig = {
     connectionString: "postgres://ahhptrdq:4pfQuAEL-m0QvezkTZRDP3t3AWDIGV-d@batyr.db.elephantsql.com/ahhptrdq"
